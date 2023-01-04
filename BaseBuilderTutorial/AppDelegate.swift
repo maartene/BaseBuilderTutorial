63530a3124e209e097b7e19a84125ce4a98993ee
Maarten Engels <maartene@mac.com>
2023-01-04 13:38:07 +0100

    Initial Commit

---
+++ BaseBuilderTutorial/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BaseBuilderTutorial
+//
+//  Created by Maarten Engels on 04/01/2023.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
